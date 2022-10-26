4677661245ac0415b2612b652454d4ad8bf53fc1
devSATURNO <110566544+devSATURNO@users.noreply.github.com>
2022-10-26 19:24:57 +0100

    Initial commit

---
+++ js2/script.js
@@ -0,0 +1,4 @@
+let nome = window.prompt('Qual é o seu nome')//a variavel nome será igual ao valor inserido na caixa de dialogo
+
+
+window.alert('É um grande prazer conhecer,  ' +nome)//aparecerá logo após o usúario fechar a janela no prompt e dirá o valor colocado pelo usúaria
---
+++ js2/script2.js
@@ -0,0 +1,12 @@
+//const n1 =Number.parseFloat( window.prompt('Digite um númer: '))
+//const n2 =Number.parseFloat( window.prompt('Digite outro número: '))
+
+const n2 =Number( window.prompt('Digite um número: '))
+const n3 =Number( window.prompt('Digite outro número: '))
+
+
+let soma = n1+n2
+
+window.alert('A sua soma é: ' + soma)
+// + (number+number) para adição
+// + (string+string)para concatenação
